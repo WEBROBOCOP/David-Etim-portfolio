cb1c93c078591f9f4e593ad79558e4e9a91585de
David <david.etim@dci-student.org>
2025-02-26 13:31:08 +0100

    updated my project and it path, need to also render the site

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,8 @@ export const projects = [
     id: 1,
     title: "Hotel-booking app",
     description: "A full-stack hotel booking application using the MERN stack",
-    technologies: ["React", "Tailwind CSS"],
-    github: "https://github.com/WEBROBOCOP/hotel-booking",
+    technologies: ["React", "Javascript", "Tailwind CSS"],
+    github: "https://git@github.com:WEBROBOCOP/hotel-booking-app.git",
     demo: "https://hotel-booking-demo.com",
     image: "/path-to-image.jpg"
   },
@@ -14,20 +14,31 @@ export const projects = [
     title: "Fullstack-E-Commerce",
     description: "A full-stack E-Commerce application using the MERN stack",
     technologies: ["React", "Node.js", "MongoDB", "Express.js", "Tailwind CSS"],
-    github: "https://github.com/WEBROBOCOP/full-stack-e-commerce",
-    demo: "https://fullstack-e-commerce site",
+    github: "https://github.com/WEBROBOCOP/FullStack-E-commerce.git",
+    demo: "https://fullstack-e-commerce-site",
     image: "/path-to-image.jpg"
   },
 
   {
     id:3,
-    title: "Fullstack-blogging-platform",
+    title: "Fullstack-Blogging-Platform",
     description: "A full-stack Blogging platform application using the MERN stack",
     technologies: ["React", "Node.js", "MongoDB", "Express.js", "Tailwind CSS"],
-    github: "https://github.com/WEBROBOCOP/full-stack-Blogging-platform",
+    github: "https://github.com/WEBROBOCOP/FullStack-Blogging-App.git",
     demo: "https://fullstack-blogging-site",
     image: "/path-to-image.jpg"
   },
 
+
+
+  {
+    id:3,
+    title: "Developer-Portfolio",
+    description: "A full-stack Portfolio for MERN Developer",
+    technologies: ["React", "Node.js", "framer-motion", "Express.js", "Tailwind CSS"],
+    github: "https://github.com/WEBROBOCOP/David-Etim-portfolio.git",
+    demo: "https://david-etim-portfolio.onrender.com/",
+    image: "/path-to-image.jpg"
+  },
   // Add more projects...
 ]; 
